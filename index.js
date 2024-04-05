31880b0f5294f9f2e3b77f9d390ffd43704d5363
Maria Kousar <160174105+Maria310306@users.noreply.github.com>
2024-04-06 02:41:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, // Base Currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let user_input = await inquirer.prompt([
+    {
+        name: "from",
+        message: "From Currency: ",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "to",
+        message: "To Currency:",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: "Enter your Amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[user_input.from];
+let toAmount = currency[user_input.to];
+let amount = user_input.amount;
+let baseAmount = amount / fromAmount; // USD base amount
+let convertedAmount = baseAmount * toAmount;
+console.log(Math.floor(convertedAmount));
